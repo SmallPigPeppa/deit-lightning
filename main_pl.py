070b2da0b27da00c296ec5225608ee7816f7c113
lwz <905328134@qq.com>
2024-07-22 02:26:21 +0800

    initial

--- main_pl.py
+++ main_pl.py
@@ -211,8 +211,8 @@ if __name__ == "__main__":
     checkpoint_callback = ModelCheckpoint(**args.model_checkpoint)
     wandb_logger = WandbLogger(**args, log_model=False)
     lr_monitor = LearningRateMonitor(**args.lr_monitor)
-    # args.trainer.logger = wandb_logger
-    # args.callbacks = [checkpoint_callback, lr_monitor]
+    args.trainer.logger = wandb_logger
+    args.callbacks = [checkpoint_callback, lr_monitor]
     trainer = pl.Trainer(**args.trainer)
     model = DeiTModel(args)
     # train_loader, val_loader = get_loaders(args)
