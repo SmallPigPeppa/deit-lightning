5b08898b613ebce10c9dd63c621543464b783add
lwz <905328134@qq.com>
2024-07-22 10:30:53 +0800

    initial

---
+++ train_npu.sh
@@ -0,0 +1,12 @@
+python main_pl.py \
+    --project DeiT-PL \
+    --name DeiT \
+    --model deit_small_patch16_224 \
+    --accelerator npu \
+    --batch-size 256 \
+    --precision 16 \
+    --log_every_n_steps 1 \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --data-path /home/ma-user/work/wenzhuoliu/torch_ds/imagenet
