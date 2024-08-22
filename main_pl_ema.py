270c781dcfd38bd1cb4b7056e55d153060fddfaf
lwz <905328134@qq.com>
2024-08-22 12:21:58 +0800

    initial

--- main_pl_ema.py
+++ main_pl_ema.py
@@ -5,7 +5,7 @@ from lightning.pytorch import cli
 from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
 from lightning.pytorch.callbacks.lr_monitor import LearningRateMonitor
 from lightning.pytorch.loggers import WandbLogger
-from deit_pl import DeiTModel
+from deit_pl_ema import DeiTModel
 from datasets import build_dataset
 from augment import new_data_aug_generator
 import os
