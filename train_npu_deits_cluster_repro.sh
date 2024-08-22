9ee96dd834a006011e20813fd0a1929ef415e6fa
lwz <905328134@qq.com>
2024-08-22 17:02:06 +0800

    initial

--- train_npu_deits_cluster_repro.sh
+++ train_npu_deits_cluster_repro.sh
@@ -1,10 +1,11 @@
 MODEL_NAME=deit_small_patch16_224
-python main_pl.py \
+python main_pl_scaleLR.py \
     --project DeiT-PL \
     --name ${MODEL_NAME}-REPRO \
     --model ${MODEL_NAME} \
     --offline \
     --accelerator npu \
+    --devices ${NGPUS_PER_NODE} \
     --num_nodes ${NNODES} \
     --precision 16 \
     --log_every_n_steps 1 \
@@ -13,3 +14,4 @@ python main_pl.py \
     --model_checkpoint.save_weights_only True \
     --model_checkpoint.filename ${MODEL_NAME}\
     --data-path /opt/huawei/dataset/all/torch_ds/imagenet
+
