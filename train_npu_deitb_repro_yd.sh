e61e262850b33268552c085eb89a27e0bebf540a
lwz <905328134@qq.com>
2024-08-23 12:31:50 +0800

    initial

---
+++ train_npu_deitb_repro_yd.sh
@@ -0,0 +1,17 @@
+MODEL_NAME=deit_base_patch16_224
+python main_pl_scaleLR.py \
+    --project DeiT-PL \
+    --name ${MODEL_NAME}-REPRO-YD \
+    --model ${MODEL_NAME} \
+    --offline \
+    --accelerator npu \
+    --devices 8 \
+    --batch-size 128 \
+    --num_nodes 1 \
+    --precision 16 \
+    --log_every_n_steps 1 \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --model_checkpoint.filename ${MODEL_NAME}\
+    --data-path /home/ma-user/work/wenzhuoliu/torch_ds/imagenet
