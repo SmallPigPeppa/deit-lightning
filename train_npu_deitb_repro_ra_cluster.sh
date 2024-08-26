f79c3bfa305149ec65ca96a6aafea7a46f3c209b
lwz <905328134@qq.com>
2024-08-26 13:39:27 +0800

    initial

--- train_npu_deitb_repro_ra_cluster.sh
+++ train_npu_deitb_repro_ra_cluster.sh
@@ -1,15 +1,15 @@
 MODEL_NAME=deit_base_patch16_224
-python main_pl_scaleLR.py \
+python main_pl_scaleLR_ra.py \
     --project DeiT-PL \
-    --name ${MODEL_NAME}-REPRO-YD-webstudio \
+    --name ${MODEL_NAME}-REPRO-RA \
     --model ${MODEL_NAME} \
     --offline \
-    --accelerator npu \
-    --devices 8 \
     --batch-size 128 \
-    --num_nodes 1 \
-    --precision 16 \
-    --log_every_n_steps 1 \
+    --trainer.accelerator npu \
+    --trainer.devices 8 \
+    --trainer.num_nodes 1 \
+    --trainer.precision 16 \
+    --trainer.log_every_n_steps 1 \
     --lr_monitor.logging_interval epoch \
     --model_checkpoint.dirpath ckpt \
     --model_checkpoint.save_weights_only True \
