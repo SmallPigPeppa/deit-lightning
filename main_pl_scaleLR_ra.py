9103bc618a336f54bb1d5bf321f46b4973d77b1b
lwz <905328134@qq.com>
2024-08-26 13:46:57 +0800

    initial

--- main_pl_scaleLR_ra.py
+++ main_pl_scaleLR_ra.py
@@ -183,9 +183,10 @@ if __name__ == "__main__":
 
     # scale learning rate
     if not args.unscale_lr:
-        scale_factor = args.batch_size * args.trainer.devices * args.trainer.num_nodes / 512.0
         print(args.trainer.num_nodes)
         print(args.trainer.devices)
+        scale_factor = args.batch_size * args.trainer.devices * args.trainer.num_nodes / 512.0
+
         linear_scaled_lr = args.lr * scale_factor
         args.lr = linear_scaled_lr
 
