cc1c47d7eda6f76423bcd74608ac391b210675b9
lwz <905328134@qq.com>
2024-08-27 19:23:43 +0800

    initial

--- train_npu_deits_repro_yd.sh
+++ train_npu_deits_repro_yd.sh
@@ -13,4 +13,4 @@ python main_pl_scaleLR.py \
     --model_checkpoint.dirpath ckpt \
     --model_checkpoint.save_weights_only True \
     --model_checkpoint.filename ${MODEL_NAME}\
-    --data-path /home/ma-user/work/wenzhuoliu/torch_ds/imagenet
+    --data-path /home/ma-user/work/dataset/all/torch_ds/imagenet
