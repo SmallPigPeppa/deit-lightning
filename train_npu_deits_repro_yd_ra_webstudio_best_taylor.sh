4bd4ddbded8702748680c714e80a6cecd08fcbec
lwz <905328134@qq.com>
2024-10-30 00:23:10 +0800

    initial

--- train_npu_deits_repro_yd_ra_webstudio_best_taylor.sh
+++ train_npu_deits_repro_yd_ra_webstudio_best_taylor.sh
@@ -1,5 +1,5 @@
 MODEL_NAME=deit_small_patch16_224
-MODEL_NAME=deit_base_patch16_224
+#MODEL_NAME=deit_base_patch16_224
 python main_pl_scaleLR_ra_taylor.py \
     --epochs 1 \
     --lr 5e-10 \
@@ -17,4 +17,4 @@ python main_pl_scaleLR_ra_taylor.py \
     --model_checkpoint.dirpath ckpt \
     --model_checkpoint.save_weights_only True \
     --model_checkpoint.filename ${MODEL_NAME}\
-    --data-path /home/ma-user/work/dataset/all/torch_ds/imagenet
+    --data-path /home/ma-user/work/wenzhuoliu/torch_ds/imagenet
