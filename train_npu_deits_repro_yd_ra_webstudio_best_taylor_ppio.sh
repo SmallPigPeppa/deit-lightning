289a5f58f5e44b7198c7f97d05e7984860fccfab
lwz <905328134@qq.com>
2024-10-30 01:16:07 +0800

    initial

---
+++ train_npu_deits_repro_yd_ra_webstudio_best_taylor_ppio.sh
@@ -0,0 +1,20 @@
+MODEL_NAME=deit_small_patch16_224
+#MODEL_NAME=deit_base_patch16_224
+python main_pl_scaleLR_ra_taylor.py \
+    --epochs 1 \
+    --lr 5e-10 \
+    --seed 3407 \
+    --project DeiT-PL-Taylor \
+    --name ${MODEL_NAME}-REPRO-YD-RA \
+    --model ${MODEL_NAME} \
+    --batch-size 64 \
+    --trainer.accelerator gpu \
+    --trainer.devices 1 \
+    --trainer.num_nodes 1 \
+    --trainer.precision 16 \
+    --trainer.log_every_n_steps 1 \
+    --lr_monitor.logging_interval epoch \
+    --model_checkpoint.dirpath ckpt \
+    --model_checkpoint.save_weights_only True \
+    --model_checkpoint.filename ${MODEL_NAME}\
+    --data-path /ppio_net0/torch_ds/imagenet
