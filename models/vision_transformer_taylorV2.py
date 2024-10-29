0a6537774e6496626dbd9ab51650fc45278bdbdd
lwz <905328134@qq.com>
2024-10-29 14:54:05 +0800

    initial

--- models/vision_transformer_taylorV2.py
+++ models/vision_transformer_taylorV2.py
@@ -151,16 +151,16 @@ if __name__ == "__main__":
     print(f"Original Attention Output Shape: {output_original.shape}, Time: {original_time:.6f} seconds")
 
     # 1～3 阶泰勒展开的注意力机制
-    for order in range(1, 5):
-        taylor_attn = TaylorAttention(dim=512, num_heads=8, order=order).to(device)
-        start_time = time.time()
-        output_taylor = taylor_attn(x)
-        taylor_time = time.time() - start_time
-        print(f"Order {order} Taylor Attention Output Shape: {output_taylor.shape}, Time: {taylor_time:.6f} seconds")
+    # for order in range(1, 5):
+    #     taylor_attn = TaylorAttention(dim=512, num_heads=8, order=order).to(device)
+    #     start_time = time.time()
+    #     output_taylor = taylor_attn(x)
+    #     taylor_time = time.time() - start_time
+    #     print(f"Order {order} Taylor Attention Output Shape: {output_taylor.shape}, Time: {taylor_time:.6f} seconds")
 
-    # order=5
-    # taylor_attn = TaylorAttention(dim=512, num_heads=8, order=order).to(device)
-    # start_time = time.time()
-    # output_taylor = taylor_attn(x)
-    # taylor_time = time.time() - start_time
-    # print(f"Order {order} Taylor Attention Output Shape: {output_taylor.shape}, Time: {taylor_time:.6f} seconds")
+    order=5
+    taylor_attn = TaylorAttention(dim=512, num_heads=8, order=order).to(device)
+    start_time = time.time()
+    output_taylor = taylor_attn(x)
+    taylor_time = time.time() - start_time
+    print(f"Order {order} Taylor Attention Output Shape: {output_taylor.shape}, Time: {taylor_time:.6f} seconds")
